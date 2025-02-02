da29891f9ea33aeabdd5fdf148a5fbf1f8ea3dce
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2025-02-02 07:23:44 +0500

    Add project files.

---
+++ MultiLanguage/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc().AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+var cultures = new[] { "en", "hi" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(cultures[0])
+    .AddSupportedCultures(cultures)
+    .AddSupportedUICultures(cultures);
+app.UseRequestLocalization(localizationOptions);
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
