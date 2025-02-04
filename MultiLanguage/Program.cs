115f91bb9e3eb991b720df9c6163514c72000009
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2025-02-04 07:14:14 +0500

    new

--- MultiLanguage/Program.cs
+++ MultiLanguage/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,19 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddMvc().AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var cultures = new[] List<CultureInfo>
+    {
+        new CultureInfo("en"),
+        new CultureInfo("fr"),
+        new CultureInfo("hi"),
+    };
+    options.DefaultRequestCulture = new RequestCulture("en");
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+});
+
 
 var app = builder.Build();
 
@@ -18,14 +36,18 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+  
 app.UseRouting();
+         
+//var cultures = new[] { "en", "hi" };
+//var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(cultures[0])
+//    .AddSupportedCultures(cultures)
+//    .AddSupportedUICultures(cultures);
+app.UseRequestLocalization(app.Services.CreateScope().ServiceProvider.
+    GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 
-var cultures = new[] { "en", "hi" };
-var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(cultures[0])
-    .AddSupportedCultures(cultures)
-    .AddSupportedUICultures(cultures);
-app.UseRequestLocalization(localizationOptions);
+
+
 
 app.UseAuthorization();
 
